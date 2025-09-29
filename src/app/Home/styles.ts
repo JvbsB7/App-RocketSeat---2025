e19ab5065d981a381a6403a54e87224a07352550
JvbsB7 <jvbrito72@gmail.com>
2025-09-29 14:04:47 -0300

    Adicionando novos arquivos

---
+++ src/app/Home/styles.ts
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: "blue"
+  },
+})
