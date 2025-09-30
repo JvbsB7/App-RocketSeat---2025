b5a0539a101d0cc5aff1891014847b46af350863
JvbsB7 <jvbrito72@gmail.com>
2025-09-30 13:41:57 -0300

    Adicionando componente Input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -4,11 +4,16 @@ import { Button } from '@/components/Button';
 
 import { styles } from './styles';
 
+import { Input } from '@/components/Input';
+
 export function Home(){
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo}/>
-      <Button />
+
+      <Input placeholder='O que você precisa comprar?' />
+
+      <Button title="Adicionar" />
     </View>
   )
 }
---
+++ src/components/Input/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ffffff',
+        height: 48,
+        width: "100%",
+        borderRadius: 8,
+        paddingHorizontal: 16,
+        borderWidth: 1,
+        borderColor: '#C3C5CB',
+    }
+})
