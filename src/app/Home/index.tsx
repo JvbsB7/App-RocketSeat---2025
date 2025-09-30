46a8ac1b085eab7ed2595867b656b05e7c330bff
JvbsB7 <jvbrito72@gmail.com>
2025-09-30 13:51:01 -0300

    Adicionado content

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -11,9 +11,12 @@ export function Home(){
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo}/>
 
+      <View style={styles.form}>
       <Input placeholder='O que você precisa comprar?' />
-
       <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   )
 }
